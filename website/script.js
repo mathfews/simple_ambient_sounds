cf20fb727b95b03c8086b274e5cf8096bb94cb8a
Matheus Felipe <matheusfcw1010@gmail.com>
2026-01-14 00:15:47 -0300

    add audio files and create the background player feature

--- website/script.js
+++ website/script.js
@@ -2,9 +2,33 @@ const forest = document.getElementById("forest")
 const wave = document.getElementById("wave")
 const library = document.getElementById("library")
 const boxes = document.querySelectorAll(".box")
-var lock = false
+const audioWave = document.getElementById("audio-wave")
+const audioForest = document.getElementById("audio-forest")
+const audioLibrary = document.getElementById("audio-library")
+
+let lock = false
+let currentSong = null
 boxes.forEach(box => {
     box.addEventListener("click", () => {
+        let nextAudio = null
+
+        if (box.id === "wave") nextAudio = audioWave
+        if (box.id === "forest") nextAudio = audioForest
+        if (box.id === "library") nextAudio = audioLibrary
+
+        if (currentSong === null || currentSong.paused) {
+            nextAudio.play()
+            currentSong = nextAudio
+        } else if (currentSong == nextAudio) {
+            nextAudio.pause()
+        } else if (currentSong != null && currentSong != nextAudio) {
+            currentSong.pause()
+            currentSong.currentTime = 0
+            currentSong = nextAudio
+            nextAudio.play()
+        }
+
+
         boxes.forEach(box => {
             document.getElementById(box.id).style.transition = "box-shadow 0.2s ease;"
             box.classList.remove("selected")
@@ -14,13 +38,9 @@ boxes.forEach(box => {
         return lock = true
     })
     box.addEventListener("mouseleave", () => {
-        console.log(lock)
         if (lock == false) {
             document.body.style.transition = "background-color 0.5s ease"
             document.documentElement.setAttribute("data-theme", "default")
-        }
-        if (lock == true) {
-            console.log("locked")
         }
     })
 
